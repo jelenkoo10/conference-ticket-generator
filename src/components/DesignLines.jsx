bb3523c2bded44e913a129d026e86979cb903fbb
jelenkoo10 <veljkojelenkovic00@gmail.com>
2025-02-18 19:21:59 +0100

    Setting up page background

---
+++ src/components/DesignLines.jsx
@@ -0,0 +1,22 @@
+export default function DesignLines() {
+  return (
+    <>
+      <img src="../assets/images/pattern-lines.svg" alt="Pattern lines" />
+      <img
+        src="../assets/images/pattern-circle.svg"
+        alt="Pattern circle"
+        className="absolute top-[50%] right-[-20px] w-20"
+      />
+      <img
+        src="../assets/images/pattern-squiggly-line-top.svg"
+        alt="Squiggly top line"
+        className="absolute right-0 top-6 w-1/4"
+      />
+      <img
+        src="../assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg"
+        alt="Squiggly bottom line"
+        className="absolute left-0 bottom-0 w-2/3"
+      />
+    </>
+  );
+}
